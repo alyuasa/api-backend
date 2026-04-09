168542751ce8eae83589ca599d7a1cda2b89eff8
alyuasa <yuasalicia@gmail.com>
2026-04-09 16:26:24 -0300

    estrutura básica entrega 2

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ app.use(express.json());
 const userRoutes = require('./routes/users');
 const pilotRoutes = require('./routes/corredores');
 const lapRoutes = require('./routes/voltas');
+const dashRoutes = require('./routes/dashboard');
 
 app.use('/usuarios', userRoutes);
 app.use('/corredores', pilotRoutes);
 app.use('/voltas', lapRoutes);
+app.use('/dashboard', dashRoutes);
 
 module.exports = app;
---
+++ routes/dashboard.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = express.Router();
+const db = require('../db');
+
+// melhor volta (individual)
+
+// tempo total 
+
+// qtd de voltas
+
+// ranking geral
+
+// estatísticas corredor específico
