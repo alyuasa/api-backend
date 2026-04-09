bd273e71740d3e5faeee8d00f80aa93adac197d2
alyuasa <yuasalicia@gmail.com>
2026-04-09 15:05:45 -0300

    colchetes na rota create corredor

--- routes/corredores.js
+++ routes/corredores.js
@@ -4,7 +4,7 @@ const db = require('../db');
 
 // create
 routes.post('/create', (req, res) => {
-    const [nome, turma] = req.body;
+    const {nome, turma} = req.body;
     const query = 'INSERT INTO corredores (nome, turma) VALUES (?, ?)';
 
     db.query(query, [nome, turma], (err, results) => {
